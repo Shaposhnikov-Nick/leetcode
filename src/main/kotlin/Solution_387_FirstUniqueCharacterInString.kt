c509d72fb7a55d3957b1b88978e0c3fc1520ffbc
shaposhnikov.nick <shaposhnikov.nick@gmail.com>
2025-11-11 16:31:33 +0300

    upd description

--- src/main/kotlin/Solution_387_FirstUniqueCharacterInString.kt
+++ src/main/kotlin/Solution_387_FirstUniqueCharacterInString.kt
@@ -1,6 +1,8 @@
 /**
  * 387 First Unique Character in a String
  *
+ * Easy
+ * 
  * Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
  *
  *
